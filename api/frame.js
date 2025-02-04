eba2d8d5ad19c0fe11f5f59954b4c33ff4799944
Deepakjuneja28 <deepakjuneja2001@gmail.com>
2025-02-04 15:14:23 +0530

    Fix API route for Vercel

--- api/frame.js
+++ api/frame.js
@@ -1,24 +1,19 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-
-  res.json({
-    frame: {
-      version: "vNext",
-      image: "https://i.ibb.co/60h9VH1M/Virat-Kohli-Dp-Hd.jpg",
-      buttons: [
-        {
-          label: "Button 1",
-          action: "post",
-        },
-        {
-          label: "Visit Link",
-          action: "link",
-          target: "https://wikipedia.org",
-        },
-      ],
-      postUrl: "https://farcaster-frame-plum.vercel.app",
-    },
-  });
-}
+module.exports = async (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  
+    res.status(200).json({
+      frame: {
+        version: "vNext",
+        image: "https://i.ibb.co/60h9VH1M/Virat-Kohli-Dp-Hd.jpg",
+        buttons: [
+          {
+            label: "Visit Website",
+            action: "link",
+            target: "https://en.wikipedia.org/wiki/Virat_Kohli"
+          }
+        ]
+      }
+    });
+  };
+  
