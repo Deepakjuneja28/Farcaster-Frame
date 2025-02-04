69ef0579e5f70e8c68a8e42bd8ba62fb42ff4195
Deepakjuneja28 <deepakjuneja2001@gmail.com>
2025-02-04 15:06:08 +0530

    added endpoint

--- api/frame.js
+++ api/frame.js
@@ -1,19 +1,24 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
-  
+
   res.json({
     frame: {
       version: "vNext",
-      image: "https://i.ibb.co/60h9VH1M/Virat-Kohli-Dp-Hd.jpg", 
+      image: "https://i.ibb.co/60h9VH1M/Virat-Kohli-Dp-Hd.jpg",
       buttons: [
         {
-          label: "Visit Website",
+          label: "Button 1",
+          action: "post",
+        },
+        {
+          label: "Visit Link",
           action: "link",
-          target: "https://en.wikipedia.org/wiki/Virat_Kohli"
-        }
-      ]
-    }
+          target: "https://wikipedia.org",
+        },
+      ],
+      postUrl: "https://farcaster-frame-plum.vercel.app",
+    },
   });
 }
